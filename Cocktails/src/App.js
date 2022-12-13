df1bf36f71cee48f7b5d84a6df8433ed822e1168
eternale88 <ethanrobbins@ymail.com>
2022-12-13 14:29:51 -0500

    upgrade to react router 6

--- Cocktails/src/App.js
+++ Cocktails/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // import pages
 import Home from './pages/Home'
 import About from './pages/About'
@@ -12,22 +12,14 @@ function App() {
   return (
     <div>
       <Router>
-      {/*navbar not added to switch as we want it to always be there*/}
-        <Navbar/>
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route path="/about">
-            <About/>
-          </Route>
-          <Route path="/cocktail/:id">
-            <SingleCocktail/>
-          </Route>
-          <Route path="*">
-            <Error/>
-          </Route>
-        </Switch>
+        {/*navbar not added to switch as we want it to always be there*/}
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cocktail/:id" element={<SingleCocktail />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
       </Router>
     </div>
   )
--- Cocktails/src/components/SearchForm.js
+++ Cocktails/src/components/SearchForm.js
@@ -4,26 +4,31 @@ import { useGlobalContext } from '../context'
 const SearchForm = () => {
   // going with uncontrolled input for a change, so we will use useRef to get values
   const searchValue = React.useRef('')
-  const {setSearchTerm} = useGlobalContext()
+  const { setSearchTerm } = useGlobalContext()
 
   const searchCocktail = () => {
     setSearchTerm(searchValue.current.value)
   }
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault()
   }
 
   //setup auto focus on input
-React.useEffect(() => {
-  searchValue.current.focus()
-}, [])
+  React.useEffect(() => {
+    searchValue.current.focus()
+  }, [])
   return (
     <section className="section search">
       <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search for your favorite cocktail</label>
-          <input type="text" id="name" ref={searchValue} onChange={searchCocktail}/>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
         </div>
       </form>
     </section>
